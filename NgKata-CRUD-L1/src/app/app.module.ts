c9890f1822cc81c8b532d8017aee8b76455b9b3a
Borcze Angelov <Borcze.Angelov@yahoo.com>
2021-10-30 13:36:46 +0200

    create PersonDataService

--- NgKata-CRUD-L1/src/app/app.module.ts
+++ NgKata-CRUD-L1/src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { PersonShellComponent } from './persons/person-shell/person-shell.component';
 import { PersonListComponent } from './persons/person-list/person-list.component';
 import { PersonEditComponent } from './persons/person-edit/person-edit.component';
+import { PersonDataService } from './persons/person-data.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,9 @@ import { PersonEditComponent } from './persons/person-edit/person-edit.component
     PersonEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(PersonDataService),
   ],
   providers: [],
   bootstrap: [AppComponent]
