da4b6f311e24b053eb72dfa06ff101590f6d05ce
Borcze Angelov <Borcze.Angelov@yahoo.com>
2021-10-30 13:17:09 +0200

    create 3 persons components

--- NgKata-CRUD-L1/src/app/app.module.ts
+++ NgKata-CRUD-L1/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { PersonShellComponent } from './persons/person-shell/person-shell.component';
+import { PersonListComponent } from './persons/person-list/person-list.component';
+import { PersonEditComponent } from './persons/person-edit/person-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    PersonShellComponent,
+    PersonListComponent,
+    PersonEditComponent
   ],
   imports: [
     BrowserModule
---
+++ NgKata-CRUD-L1/src/app/persons/person-shell/person-shell.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-person-shell',
+  templateUrl: './person-shell.component.html'
+})
+export class PersonShellComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
